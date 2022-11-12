65a8686a7a61cfe5564440e452ff387cf8d07dfb
rexthecapt <rexthecapt@gmail.com>
2022-11-12 23:14:08 +0100

    Combine all json objects in all journal files into a single array

---
+++ EdJournalArrayer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EdJournalArrayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string journalPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\Saved Games\\Frontier Developments\\Elite Dangerous";
+            string[] journals = Directory.GetFiles(journalPath, "Journal.*.log").OrderBy(x=>(new FileInfo(x)).LastWriteTime).ToArray();
+
+            JArray a = new();
+
+            foreach (string journal in journals)
+                foreach (string line in File.ReadAllLines(journal))
+                    a.Add((JObject)JsonConvert.DeserializeObject(line));
+
+            string raw = a.ToString();
+
+            using (StreamWriter sw = new("output.json"))
+                sw.WriteLine(raw);
+
+            Console.WriteLine(Path.GetFullPath("output.json"));
+        }
+    }
+}
